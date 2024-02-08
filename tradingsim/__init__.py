769db3f50337e401d0fcf6457e82a646ea72d625
guerra0911 <guerran@mcmaster.ca>
2024-02-07 22:46:35 -0500

    PostgreSQL

--- tradingsim/__init__.py
+++ tradingsim/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)       #app = class of Flask
 app.config['SECRET_KEY'] = '2134a260716814ec7c9d781f885ef9e1'   #Protect Against Cyber Attacks, etc. Will make environment variable later on
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ferrari11@localhost:5432/GUERRA'
 db = SQLAlchemy(app)        #Have an SQL Alchemy Instance called db
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
